160d3ed42901fa7e2b3a8e80a7625ffbf40a8c30
haticecs <57989556+haticecs@users.noreply.github.com>
2021-08-20 21:33:50 +0300

    timer hook is added for timer functionality

--- src/components/InfoBar/index.js
+++ src/components/InfoBar/index.js
@@ -9,21 +9,31 @@ import InfoSection from "./info-section";
 import AvTimerIcon from "@material-ui/icons/AvTimer";
 import EmojiEventsIcon from "@material-ui/icons/EmojiEvents";
 import LoopIcon from "@material-ui/icons/Loop";
+import useTimer from "../hooks/useTimer";
 
-const InfoBar = () => (
-  <Container>
-    <InfoSectionWrapper>
-      <InfoSection Icon={AvTimerIcon} InfoText="00:00:39" color="#E10600" />
-      <InfoSection Icon={EmojiEventsIcon} InfoText="0" color="#E10600" />
-    </InfoSectionWrapper>
-    <StyledText>Highest Score Ever: 1450</StyledText>
-    <InfoSection
-      Icon={LoopIcon}
-      InfoText="RESTART"
-      color="#E10600"
-      isClickable
-      onClick={() => console.log("clicked")}
-    />
-  </Container>
-);
+const InfoBar = () => {
+  //custom hook
+  const [hours, minutes, seconds] = useTimer();
+  let hoursStr = hours < 10 ? `0${hours}` : `${hours}`;
+  let minutesStr = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  let secondsStr = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+  let timerText = `${hoursStr}:${minutesStr}:${secondsStr}`;
+  return (
+    <Container>
+      <InfoSectionWrapper>
+        <InfoSection Icon={AvTimerIcon} InfoText={timerText} color="#E10600" />
+        <InfoSection Icon={EmojiEventsIcon} InfoText="0" color="#E10600" />
+      </InfoSectionWrapper>
+      <StyledText>Highest Score Ever: 1450</StyledText>
+      <InfoSection
+        Icon={LoopIcon}
+        InfoText="RESTART"
+        color="#E10600"
+        isClickable
+        onClick={() => console.log("clicked")}
+      />
+    </Container>
+  );
+};
 export default InfoBar;
---
+++ src/components/hooks/useTimer.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useTimer = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds === 59) {
+        setSeconds(0);
+        setMinutes(minutes + 1);
+      }
+      if (seconds < 59) {
+        setSeconds(seconds + 1);
+      }
+      if (minutes === 59) {
+        setMinutes(0);
+        setHours(hours + 1);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
+  return [hours, minutes, seconds];
+};
+
+export default useTimer;
